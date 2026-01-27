b7972662a469f4f837ff9936b7c18b0de1b480e2
Cameron Smith <Cs11@usf.edu>
2026-01-27 17:03:22 -0500

    Created pricing_assistant.js and Initialized new information into pricing_assistant.js

---
+++ pricing_assistant.js
@@ -0,0 +1,22 @@
+let productName = ("Coffee Mug");
+let costPerUnit = ("13.75");
+let basePrice = ("15.55");
+let discountRate = ("0.35");
+let salesTaxRate = ("0.05");
+let fixedMonthlyCosts = ("rent = $750");
+
+
+discountedPrice = 15.55 * (1 - 0.35);
+finalPriceWithTax = 10.1075 * (1 + 0.5);
+profitPerUnit = 10.612875 - 13.75;
+breakEvenUnits = Math.ceil(750 / -3.137);
+isProfitablePerUnit = (-3.137 > 0);
+
+
+console.log(productName);
+console.log(discountedprice);
+console.log(finalPriceWithTax);
+console.log(profitPerUnit);
+console.log(breakEvenUnits);
+console.log(isProfitablePerUnit);
+
